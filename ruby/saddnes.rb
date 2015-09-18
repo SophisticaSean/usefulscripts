72e394fb4636a27ed44b9f04607516971f5b0069
Sean Lewis <slewis@instructure.com>
2015-09-18 14:57:53 -0600

    made it play sounds

--- ruby/saddnes.rb
+++ ruby/saddnes.rb
@@ -1,6 +1,4 @@
-def rofl(model_number, frequency = 1)
-  model_number = ARGV[0]
-  nowinstock_path = ARGV[1]
+def rofl(model_number, nowinstock_path, frequency = 1)
   a = `wget -q -O temp.html http://www.nowinstock.net/#{nowinstock_path}; sed -n '1,10000000000p' temp.html > blarp.txt; ag "#{model_number}" blarp.txt; rm blarp.txt`
   blarp = []
   blarp << a.scan(/(Amazon|Newegg|TigerDirect|NCIXUS)(?=\s\<\/a).*stockStatus"\>(.*)(?=\<\/td\>\<td)/) #.join(" ")
@@ -15,6 +13,7 @@ def rofl(model_number, frequency = 1)
   new.each do |new_item|
     if new_item.include? "In Stock"
       p new_item
+      `afplay some_sound.mp3`
     end
   end
 
@@ -22,7 +21,7 @@ def rofl(model_number, frequency = 1)
   sleep frequency.to_i
 end
 
-10.times do
+while true do
   rofl(ARGV[0], ARGV[1])
 end
 
